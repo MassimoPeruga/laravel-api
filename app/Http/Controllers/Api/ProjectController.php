90fe8e7ce49836da68f88e5cf304230c894d28be
MassimoPeruga <spamma-mutevole0p@icloud.com>
2024-02-19 21:10:43 +0100

    feat: api search

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,11 +10,27 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('type', 'technologies')->paginate(10);
-        return response()->json([
-            'success' => true,
-            'results' => $projects,
+        request()->validate([
+            'key' => ['nullable', 'string', 'min:3']
         ]);
+
+        if (request()->key) {
+            $projects = Project::where('name', 'LIKE', '%' . request()->key . '%')->with('type', 'technologies')->paginate(10);
+        } else {
+            $projects = Project::with('type', 'technologies')->paginate(10);
+        }
+
+        if ($projects) {
+            return response()->json([
+                'success' => true,
+                'results' => $projects,
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'results' => [],
+            ]);
+        }
     }
 
     public function show(string $slug)
